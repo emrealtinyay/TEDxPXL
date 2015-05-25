dec9480802e5185dab24c32e0cba040d18b037fe
emrealtinyay <emre.altinyay@student.pxl.be>
2015-05-25 14:44:53 +0200

    wijzigingen

--- application/views/team_view.php
+++ application/views/team_view.php
@@ -23,11 +23,11 @@
 										<div><img src="<?php echo base_url();?>images/team_foto1.png" alt="team foto" class="img-team"></div><!-- pic1.jpg -->
 										<div class="boxContent color4">
 											<h3>Elvis Presley</h3>
-											<p>Praeses</p>
+											<p>Director</p>
 										</div>
-										<div class="boxContent"> <a href="mailto:jordi.ijzendoorn@student.pxl.be">jordi.ijzendoorn@student.pxl.be</a>
+										<div class="boxContent"> <a href="mailto:elvis.presley@student.pxl.be">elvis.presley@student.pxl.be</a>
 											<ul class="socialNetwork mt15">
-												<li><a href="https://www.facebook.com/jordi.ijzendoorn" class="tips" title="" data-original-title="follow me on Facebook"><i class="icon-facebook-2"></i></a></li>
+												<li><a href="https://www.facebook.com/elvis.presley" class="tips" title="" data-original-title="follow me on Facebook"><i class="icon-facebook-2"></i></a></li>
 												<li><a href="#" class="tips" title="" data-original-title="follow me on Twitter"><i class="icon-twitter-2"></i></a></li>
 												<li><a href="#" class="tips" title="" data-original-title="follow me on Google+"><i class="icon-google-2"></i></a></li>
 												<li><a href="#" class="tips" title="" data-original-title="follow me on Linkedin"><i class="icon-linkedin-2"></i></a></li>
@@ -41,12 +41,12 @@
 									<article>
 										<div><img src="<?php echo base_url();?>images/team_foto2.png" alt="team foto" class="img-team"></div>
 										<div class="boxContent color4">
-											<h3>Jory 'Djuke' Jackmaer</h3>
-											<p>Vice-praeses</p>
+											<h3>Jimmy Hendrix</h3>
+											<p>Co-director</p>
 										</div>
-										<div class="boxContent"> <a href="mailto:jory.jackmaer@student.pxl.be">jory.jackmaer@student.pxl.be</a>
+										<div class="boxContent"> <a href="mailto:jimmy.hendrix@student.pxl.be">jimmy.hendrix@student.pxl.be</a>
 											<ul class="socialNetwork mt15">
-												<li><a href="https://www.facebook.com/jory.jackmaer" class="tips" title="" data-original-title="follow me on Facebook"><i class="icon-facebook-2"></i></a></li>
+												<li><a href="https://www.facebook.com/jimmy.hendrix" class="tips" title="" data-original-title="follow me on Facebook"><i class="icon-facebook-2"></i></a></li>
 												<li><a href="#" class="tips" title="" data-original-title="follow me on Twitter"><i class="icon-twitter-2"></i></a></li>
 												<li><a href="#" class="tips" title="" data-original-title="follow me on Google+"><i class="icon-google-2"></i></a></li>
 												<li><a href="#" class="tips" title="" data-original-title="follow me on Linkedin"><i class="icon-linkedin-2"></i></a></li>
@@ -60,12 +60,12 @@
 									<article>
 										<div><img src="<?php echo base_url();?>images/team_foto3.png" alt="team foto" class="img-team"></div>
 										<div class="boxContent color4">
-											<h3>Sander 'Paaij' Screurs</h3>
-											<p>Quaestor</p>
+											<h3>Lionel Messi</h3>
+											<p>Artist</p>
 										</div>
-										<div class="boxContent"> <a href="mailto:sander.screurs@student.pxl.be">sander.screurs@student.pxl.be</a>
+										<div class="boxContent"> <a href="mailto:lionel.messi@student.pxl.be">lionel.messi@student.pxl.be</a>
 											<ul class="socialNetwork mt15">
-												<li><a href="https://www.facebook.com/sander.screurs" class="tips" title="" data-original-title="follow me on Facebook"><i class="icon-facebook-2"></i></a></li>
+												<li><a href="https://www.facebook.com/lionel.messi" class="tips" title="" data-original-title="follow me on Facebook"><i class="icon-facebook-2"></i></a></li>
 												<li><a href="#" class="tips" title="" data-original-title="follow me on Twitter"><i class="icon-twitter-2"></i></a></li>
 												<li><a href="#" class="tips" title="" data-original-title="follow me on Google+"><i class="icon-google-2"></i></a></li>
 												<li><a href="#" class="tips" title="" data-original-title="follow me on Linkedin"><i class="icon-linkedin-2"></i></a></li>
@@ -79,12 +79,12 @@
 									<article>
 										<div><img src="<?php echo base_url();?>images/team_foto4.png" alt="team foto" class="img-team"></div>
 										<div class="boxContent color4">
-											<h3>Diego Deiana</h3>
-											<p>Ab-actis</p>
+											<h3>Elton John</h3>
+											<p>Artist</p>
 										</div>
-										<div class="boxContent"> <a href="mailto:diego.deiana@student.pxl.be">diego.deiana@student.pxl.be</a>
+										<div class="boxContent"> <a href="mailto:elton.john@student.pxl.be">elton.john@student.pxl.be</a>
 											<ul class="socialNetwork mt15">
-												<li> <a href="https://www.facebook.com/Don.Diego.Deiana" class="tips" title="" data-original-title="follow me on Facebook"><i class="icon-facebook-2"></i></a> </li>
+												<li> <a href="https://www.facebook.com/elton.john" class="tips" title="" data-original-title="follow me on Facebook"><i class="icon-facebook-2"></i></a> </li>
 												<li> <a href="#" class="tips" title="" data-original-title="follow me on Twitter"><i class="icon-twitter-2"></i></a> </li>
 												<li> <a href="#" class="tips" title="" data-original-title="follow me on Google+"><i class="icon-google-2"></i></a> </li>
 												<li> <a href="#" class="tips" title="" data-original-title="follow me on Linkedin"><i class="icon-linkedin-2"></i> </a> </li>
@@ -92,195 +92,7 @@
 											</ul>
 										</div>
 									</article>
-								</div>
-                                <div class="col-sm-6 col-md-3">
-
-<article>
-
-<div><img src="<?php echo base_url();?>images/team_foto5.png" alt="team foto" class="img-team"></div>
-
-<div class="boxContent color4">
-
-<h3>Wouter Bollaert</h3>
-
-<p>Schachtenmeester</p>
-
-</div>
-
-<div class="boxContent"> <a href="mailto:wouter.bollaert@student.pxl.be">wouter.bollaert@student.pxl.be</a>
-
-<ul class="socialNetwork mt15">
-
-<li> <a href="https://www.facebook.com/TheGreenGrasshoppper" class="tips" title="" data-original-title="follow me on Facebook"><i class="icon-facebook-2"></i></a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Twitter"><i class="icon-twitter-2"></i></a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Google+"><i class="icon-google-2"></i></a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Linkedin"><i class="icon-linkedin-2"></i> </a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Instagram"><i class="icon-instagram-2"></i> </a> </li>
-
-</ul>
-
-</div>
-
-                                    </article>
-
-                                </div>
-
-                                
-
-<div class="col-sm-6 col-md-3">
-
-<article>
-
-<div><img src="<?php echo base_url();?>images/team_foto6.png" alt="team foto" class="img-team"></div>
-
-<div class="boxContent color4">
-
-<h3>Siemen Hermans</h3>
-
-<p>Doopmeester</p>
-
-</div>
-
-<div class="boxContent"> <a href="mailto:siemen.hermans@student.pxl.be">siemen.hermans@student.pxl.be</a>
-
-<ul class="socialNetwork mt15">
-
-<li> <a href="https://www.facebook.com/siemen.hermans" class="tips" title="" data-original-title="follow me on Facebook"><i class="icon-facebook-2"></i></a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Twitter"><i class="icon-twitter-2"></i></a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Google+"><i class="icon-google-2"></i></a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Linkedin"><i class="icon-linkedin-2"></i> </a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Instagram"><i class="icon-instagram-2"></i> </a> </li>
-
-</ul>
-
-</div>
-
-                                    </article>
-
-                                </div>
-
-                                
-
-<div class="col-sm-6 col-md-3">
-
-<article>
-
-<div><img src="<?php echo base_url();?>images/team_foto7.png" alt="team foto" class="img-team"></div>
-
-<div class="boxContent color4">
-
-<h3>Ruben Bielen</h3>
-
-<p>Bacchus</p>
-
-</div>
-
-<div class="boxContent"> <a href="mailto:ruben.bielen@student.pxl.be">ruben.bielen@student.pxl.be</a>
-
-<ul class="socialNetwork mt15">
-
-<li> <a href="https://www.facebook.com/RubenBielen" class="tips" title="" data-original-title="follow me on Facebook"><i class="icon-facebook-2"></i></a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Twitter"><i class="icon-twitter-2"></i></a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Google+"><i class="icon-google-2"></i></a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Linkedin"><i class="icon-linkedin-2"></i> </a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Instagram"><i class="icon-instagram-2"></i> </a> </li>
-
-</ul>
-
-</div>
-
-                                    </article>
-
-                                </div>
-
-                                                          
-
-<div class="col-sm-6 col-md-3">
-
-<article>
-
-<div><img src="<?php echo base_url();?>images/team_foto8.png" alt="team foto" class="img-team"></div>
-
-<div class="boxContent color4">
-
-<h3>Tine Eykmans</h3>
-
-<p>Sport en Ontspanning</p>
-
-</div>
-
-<div class="boxContent"> <a href="mailto:tine.eykmans@student.pxl.be">tine.eykmans@student.pxl.be</a>
-
-<ul class="socialNetwork mt15">
-
-<li> <a href="https://www.facebook.com/eykmanstine" class="tips" title="" data-original-title="follow me on Facebook"><i class="icon-facebook-2"></i></a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Twitter"><i class="icon-twitter-2"></i></a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Google+"><i class="icon-google-2"></i></a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Linkedin"><i class="icon-linkedin-2"></i> </a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Instagram"><i class="icon-instagram-2"></i> </a> </li>
-
-</ul>
-
-</div>
-
-                                       </article>
-
-                                </div>
-
-                                
-
-<div class="col-sm-6 col-md-3">
-
-<article>
-
-<div><img src="<?php echo base_url();?>images/team_foto9.png" alt="team foto" class="img-team"></div>
-
-<div class="boxContent color4">
-
-<h3>Bart Moelans</h3>
-
-<p>PR-Externa</p>
-
-</div>
-
-<div class="boxContent"> <a href="mailto:bart.moelans@pxl.be">bart.moelans@pxl.be</a>
-
-<ul class="socialNetwork mt15">
-
-<li> <a href="https://www.facebook.com/bart.moelans" class="tips" title="" data-original-title="follow me on Facebook"><i class="icon-facebook-2"></i></a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Twitter"><i class="icon-twitter-2"></i></a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Google+"><i class="icon-google-2"></i></a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Linkedin"><i class="icon-linkedin-2"></i> </a> </li>
-
-<li> <a href="#" class="tips" title="" data-original-title="follow me on Instagram"><i class="icon-instagram-2"></i> </a> </li>
-
-</ul>
-
-</div>
-
-</article>
-
-</div>
-								
+								</div>								
 							</div>
 						</div>
 					</section>
